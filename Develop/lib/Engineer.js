660ba9d1ed6a425ecd14792a937f86119c3a5aa8
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-01-21 17:27:00 -0500

    defined and exported the rest of lib

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,14 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+class Engineer extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email)
+        this.officeNumber = officeNumber;
+
+    }
+
+    getRole() {
+        return "Engineer"
+    }
+
+
+}
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+
+class Intern extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email)
+        this.officeNumber = officeNumber;
+
+    }
+
+    getRole() {
+        return "Intern"
+    }
+
+
+}
